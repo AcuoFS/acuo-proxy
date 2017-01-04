2b5e1b330e2dff8fac1966656ede5f35362011c0
HU Ze <huze@outlook.sg>
2017-01-04 17:18:51 +0800

    added getFromCache function in dashboard service

--- app/services/Dashboard.js
+++ app/services/Dashboard.js
@@ -1,12 +1,24 @@
-//import external library
+// import external library
 const rp = require('request-promise')
+const _ = require('lodash')
+// cache
+const cache = require('../common/Cache')
 
 // main object
 const Dashboard = {}
 
 // get Dashboard items
 Dashboard.get = () => {
-  return rp('http://margin.acuo.com/acuo/api/margin/dashboard').then(JSON.parse)
+  return rp('http://margin.acuo.com/acuo/api/margin/dashboard')
+    .then(JSON.parse)
+    .then(json => {
+      cache.set('dashboard', json)
+      return json
+    })
+}
+
+Dashboard.getFromCache = () => {
+  return cache.get('dashboard').then(data => _.set(data, 'fromCache', true))
 }
 
 module.exports = Dashboard
