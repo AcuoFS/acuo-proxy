0e7231030699e62ad427cb250e53e5f1ce945558
HU Ze <huze@outlook.sg>
2017-01-04 16:02:49 +0800

    added more services

--- app/services/Dashboard.js
+++ app/services/Dashboard.js
@@ -1,20 +1,12 @@
 //import external library
-const rp = require('request-promise');
+const rp = require('request-promise')
 
 // main object
 const Dashboard = {}
 
 // get forex items
-Dashboard.getItems = () => new Promise((resolve, reject) => {
-  rp('http://margin.acuo.com/acuo/api/margin/dashboard')
-    .then((dashboard) => {
-      // successfull
-        resolve(dashboard)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-  })
-})
+Dashboard.getItems = () => {
+  return rp('http://margin.acuo.com/acuo/api/margin/dashboard').then(JSON.parse)
+}
 
 module.exports = Dashboard
--- app/services/index.js
+++ app/services/index.js
@@ -1,7 +1,9 @@
 const DashboardService = require('./Dashboard')
 const ReconService = require('./Recon')
+const PledgeService = require('./Pledge')
 
 module.exports = {
   DashboardService,
   ReconService,
+  PledgeService,
 }
