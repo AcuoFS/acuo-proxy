bbab4b8c1fcb356c3408a63168f7e8cd274e9495
PALO IT-Andrian <paloit-andrian@Palo-It-Macboook.singnet.com.sg>
2017-01-05 16:39:53 +0800

    changing recon data structure from an array to object

--- app/services/Recon.js
+++ app/services/Recon.js
@@ -1,5 +1,6 @@
 //import external library
 const rp = require('request-promise')
+// const _ = require('lodash')
 // cache
 const cache = require('../common/Cache')
 
@@ -9,7 +10,7 @@ const Recon = {}
 // get Recon items
 Recon.get = () => {
   return rp('http://margin.acuo.com/acuo/api/margin/items/all/999')
-    .then(JSON.parse)
+    .then(data => ({items: JSON.parse(data)}))
     .then(json => {
       cache.set('recon', json)
       return json
