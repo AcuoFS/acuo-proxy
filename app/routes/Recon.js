3c4bf58394c9c90a52c5bad9613e62f1ea065ffa
Rui <rchai@palo-it.com>
2017-02-06 11:05:56 +0800

    OW-382 proxy mashing data

--- app/routes/Recon.js
+++ app/routes/Recon.js
@@ -11,23 +11,39 @@ const routerInstance = new Router()
 const prefix = "recon"
 
 // ======================================================================
+// routerInstance.get('/', (req, res, next) => {
+//   const key = req.path()
+//
+//   ReconService.get().then(data => {
+//     // hit backend
+//     FsCacheService.set({key, data})
+//     res.json({items:data})
+//
+//   }).catch(err => {
+//     // hit cache
+//     FsCacheService.get(key).then(items => res.json(_.set({items}, 'fromCache', true)))
+//   })
+// })
+
 routerInstance.get('/', (req, res, next) => {
   const key = req.path()
 
   ReconService.get().then(data => {
-    // hit backend
-    FsCacheService.set({key, data})
-    res.json({items:data})
+    const newData = _.map(_.map(data.items, (item) =>
+      _.set(item, 'clientAssets', _.map(item.clientAssets, (group) =>
+        _.set(group, 'data', _.map(group.data, (firstLevel) =>
+          _.set(firstLevel, 'secondLevel', _.map(firstLevel.firstLevel.secondLevel, (secondLevel) =>
+            _.set(secondLevel, 'parentIndex', firstLevel.firstLevel.id)))))))), (item) =>
+      _.set(item, 'counterpartyAssets', _.map(item.counterpartyAssets, (group) =>
+        _.set(group, 'data', _.map(group.data, (firstLevel) =>
+          _.set(firstLevel, 'secondLevel', _.map(firstLevel.firstLevel.secondLevel, (secondLevel) =>
+            _.set(secondLevel, 'parentIndex', firstLevel.firstLevel.id))))))))
 
+    FsCacheService.set({key, newData})
+    res.json({items:newData})
   }).catch(err => {
     // hit cache
     FsCacheService.get(key).then(items => res.json(_.set({items}, 'fromCache', true)))
-  })
-})
-
-routerInstance.get('/recon-test-route', (req, res, next) => {
-  ReconService.getTestRecon().then(data => {
-    res.json({items:data.items})
   })
 })
 
