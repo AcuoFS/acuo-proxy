2c33804bc147d4e754f29d86660f321cb569105e
Jun Jie Ng <jjng@palo-it.com>
2017-02-10 10:13:44 +0800

    OW-99 New route and service

--- app/services/index.js
+++ app/services/index.js
@@ -2,10 +2,12 @@ const DashboardService = require('./Dashboard')
 const ReconService = require('./Recon')
 const PledgeService = require('./Pledge')
 const FsCacheService = require('./FsCache')
+const UnmatchedPortfolioService = require('./UnmatchedPortfolio')
 
 module.exports = {
   DashboardService,
   ReconService,
   PledgeService,
   FsCacheService,
+  UnmatchedPortfolioService
 }
