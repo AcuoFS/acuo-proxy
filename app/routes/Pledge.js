d29334176e185b72dbac388e928e12a3df33a88e
Rui <rchai@palo-it.com>
2017-03-28 11:29:52 +0800

    debugging statements

--- app/routes/Pledge.js
+++ app/routes/Pledge.js
@@ -66,8 +66,8 @@ routerInstance.post('/allocate-selection-new', (req, res, next) => {
   ]).then(data => {
     const [selectionItems, {allocated}] = data
     //console.log('allocated' + JSON.stringify(allocated))
-    console.log(JSON.stringify(selectionItems))
-    const processedItems = selectionItems.map(selectionItem => {
+    console.log(JSON.stringify(_.map(selectionItems, (item) => _.set('clientAssets', _.map(item.clientAssets, (group) => group.data.length)))))
+    const processedItems = _.map(selectionItems, (item) => _.set('clientAssets', _.map(item.clientAssets, (group) => group.data.length))).map(selectionItem => {
         _.forOwn(allocated, (allocatedInfo, allocatedGUID) => {
           if (selectionItem.GUID == allocatedGUID) {
             return _.merge(selectionItem, {
