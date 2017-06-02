8ddd824d5ca244e618789cebd03f58226ea9e753
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-06-02 07:40:01 +0100

    OW-602 adding a new config section

--- app/constants/config.js
+++ app/constants/config.js
@@ -14,6 +14,21 @@ const config = {
     GET_CURRENCY_INFO_URL : 'http://margin.acuo.com/acuo/api/margin/fxvalue/999',
     POST_REMOVE_ALLOCATED_ASSET: 'http://collateral.acuo.com/acuo/api/optimization/update'
   },
+  docker: {
+    GET_DASHBOARD_URL: 'http://traefik/margin/acuo/api/margin/dashboard',
+    GET_RECON_URL: 'http://traefik/margin/acuo/api/margin/items/all/999',
+    GET_UNMATCHED_URL: 'http://traefik/margin/acuo/api/margin/items/unmatched/999',
+    GET_OPTIMISATION_URL: 'http://traefik/margin/acuo/api/pledge/settings/optimization/999',
+    GET_PLEDGE_SELECTIONS_URL: 'http://traefik/margin/acuo/api/pledge/items/all/999',
+    GET_COLLATERAL_URL: 'http://traefik/collateral/acuo/api/assets/eligible/client/999',
+    GET_EARMARKED_COLLATERAL_URL: 'http://traefik/collateral/acuo/api/assets/reserved/client/999',
+    POST_PLEDGE_SELECTIONS_URL: 'http://traefik/collateral/acuo/api/optimization/allocate',
+    POST_PLEDGE_ALLOCATIONS_URL: 'http://traefik/margin/acuo/api/pledge/assets/transfer',
+    GET_RECON_DISPUTES_URL: 'http://traefik/margin/acuo/api/dispute/items/all/999',
+    POST_RECON_DISPUTE_URL: 'http://traefik/margin/acuo/api/dispute/add',
+    GET_CURRENCY_INFO_URL : 'http://traefik/margin/acuo/api/margin/fxvalue/999',
+    POST_REMOVE_ALLOCATED_ASSET: 'http://traefik/collateral/acuo/api/optimization/update'
+  },
   qa: {
     GET_DASHBOARD_URL: 'http://margin:7070/acuo/api/margin/dashboard',
     GET_RECON_URL: 'http://margin:7070/acuo/api/margin/items/all/999',
