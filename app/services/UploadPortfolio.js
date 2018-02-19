a58d7e16d6b3ed8e934b192b17ed51dbc733fefc
Rui <rchai@palo-it.com>
2018-02-19 13:56:40 +0800

    reduce log bloat

--- app/services/UploadPortfolio.js
+++ app/services/UploadPortfolio.js
@@ -18,8 +18,8 @@ const UploadPortfolio = {}
 UploadPortfolio.postUpload = (options, clientId) =>
   rp(`${POST_UPLOAD_PORTFOLIO}/${clientId}`, options)
     .then(response => {
-      console.log('server responded')
-      console.log(response)
+      console.log('server responded to portfolio upload')
+      // console.log(response)
       console.log(JSON.parse(response.toJSON().body))
       return JSON.parse(response.toJSON().body)
     })
